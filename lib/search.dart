746c7d56f2a0337d522d6e1d81bab15bf9a21283
wlsgur348 <wlsgur348@gmail.com>
2023-08-29 14:29:21 +0900

    [DOCS]search.dart , search_result.dart 추가

--- lib/search.dart
+++ lib/search.dart
@@ -1,6 +1,6 @@
-import 'package:anim_search_bar/anim_search_bar.dart';
 import 'package:animated_bottom_navigation_bar/animated_bottom_navigation_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:smartrecycler/search_result.dart';
 
 import 'common/colors.dart';
 
@@ -27,6 +27,8 @@ class _SearchState extends State<Search> {
 
   var _bottomNavIndex = 0;
 
+  String searchText = '';
+
   final iconList = <IconData>[
     Icons.home_filled,
     Icons.shopping_cart_outlined,
@@ -34,7 +36,26 @@ class _SearchState extends State<Search> {
     Icons.person
   ];
 
+  List<String> items = ['Item 1', 'Item 2', 'Item 3', 'Item 4'];
+  List<String> itemContents = [
+    'Item 1 Contents',
+    'Item 2 Contents',
+    'Item 3 Contents',
+    'Item 4 Contents'
+  ];
+
   TextEditingController textController = TextEditingController();
+
+  void cardClickEvent(BuildContext context, int index) {
+    String content = itemContents[index];
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        // 정의한 ContentPage의 폼 호출
+        builder: (context) => SearchResultPage(),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -58,9 +79,62 @@ class _SearchState extends State<Search> {
       body: SingleChildScrollView(
             child: Column(
               children: <Widget>[
-                Center(
-                  child: Text("data: ${widget.search}",
-                    style: TextStyle(fontSize: 24),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: widget.search,
+                      hintStyle: TextStyle(color: gray03),
+                      filled: true,
+                      fillColor: gray01,
+                      isDense: true,
+                      contentPadding: EdgeInsets.all(16),//내부 padding값 설정
+                      border: OutlineInputBorder(
+                          borderSide: BorderSide(width: 1, color:gray02),
+                          borderRadius: BorderRadius.circular(25)
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(width: 1, color:Colors.black),
+                          borderRadius: BorderRadius.circular(25)
+                      ),
+                    ),
+                    onChanged: (value) {
+                      setState(() {
+                        searchText = value;
+                      });
+                    },
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(left: 16,right: 16),
+                  height: 400,
+                  child: ListView.builder(
+                    // items 변수에 저장되어 있는 모든 값 출력
+                    itemCount: items.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      // 검색 기능, 검색어가 있을 경우
+                      if (searchText.isNotEmpty &&
+                          !items[index]
+                              .toLowerCase()
+                              .contains(searchText.toLowerCase())) {
+                        return SizedBox.shrink();
+                      }
+                      // 검색어가 없을 경우, 모든 항목 표시
+                      else {
+                        return Card(
+                          color: Colors.white,
+                          elevation: 3,
+                          shape: RoundedRectangleBorder(
+                              borderRadius:
+                              BorderRadius.all(Radius.elliptical(20, 20))),
+                          child: ListTile(
+                            title: Text(items[index]),
+                            textColor: Colors.black,
+                            onTap: () => cardClickEvent(context, index),
+                          ),
+                        );
+                      }
+                    },
                   ),
                 ),
               ],
