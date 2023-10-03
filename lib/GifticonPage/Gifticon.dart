06f753849ae15eed0bf1832a2956070df008a62d
HyeokJoon <fufu98@naver.com>
2023-10-03 16:51:46 +0900

    [Docs] GifticonRepository 작성

--- lib/GifticonPage/Gifticon.dart
+++ lib/GifticonPage/Gifticon.dart
@@ -3,20 +3,36 @@ import 'package:json_annotation/json_annotation.dart';
 part 'Gifticon.g.dart';
 
 @JsonSerializable()
-class Gifticon {
+class GifticonList{
+  List<GifticonItem>? list;
+
+  GifticonList({required this.list,});
+
+  //fromJson(json) : json파일을 매개변수로 받아서 gifticonList를 만드는 함수
+  factory GifticonList.fromJson(Map<String, dynamic> json) =>
+      _$GifticonListFromJson(json);
+
+  //toJson() : GifticonList(자신)를 매개변수로 json파일을 만드는 함수
+  Map<String, dynamic> toJson() => _$GifticonListToJson(this);
+}
+
+@JsonSerializable()
+class GifticonItem {
+  final String? gImage;
   final String? gName;
   final int? price;
   final int? stockQuantitiy;
   final String? expireData;
 
-  Gifticon({
+  GifticonItem({
+    required this.gImage,
     required this.gName,
     required this.price,
     required this.stockQuantitiy,
     required this.expireData,
   });
 
-  factory Gifticon.fromJson(Map<String, dynamic> json) => _$GifticonFromJson(json);
+  factory GifticonItem.fromJson(Map<String, dynamic> json) => _$GifticonItemFromJson(json);
 
-  Map<String, dynamic> toJson() => _$GifticonToJson(this);
+  Map<String, dynamic> toJson() => _$GifticonItemToJson(this);
 }
--- lib/GifticonPage/Gifticon.g.dart
+++ lib/GifticonPage/Gifticon.g.dart
@@ -6,14 +6,28 @@ part of 'Gifticon.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-Gifticon _$GifticonFromJson(Map<String, dynamic> json) => Gifticon(
+GifticonList _$GifticonListFromJson(Map<String, dynamic> json) => GifticonList(
+      list: (json['list'] as List<dynamic>?)
+          ?.map((e) => GifticonItem.fromJson(e as Map<String, dynamic>))
+          .toList(),
+    );
+
+Map<String, dynamic> _$GifticonListToJson(GifticonList instance) =>
+    <String, dynamic>{
+      'list': instance.list,
+    };
+
+GifticonItem _$GifticonItemFromJson(Map<String, dynamic> json) => GifticonItem(
+      gImage: json['gImage'] as String?,
       gName: json['gName'] as String?,
       price: json['price'] as int?,
       stockQuantitiy: json['stockQuantitiy'] as int?,
       expireData: json['expireData'] as String?,
     );
 
-Map<String, dynamic> _$GifticonToJson(Gifticon instance) => <String, dynamic>{
+Map<String, dynamic> _$GifticonItemToJson(GifticonItem instance) =>
+    <String, dynamic>{
+      'gImage': instance.gImage,
       'gName': instance.gName,
       'price': instance.price,
       'stockQuantitiy': instance.stockQuantitiy,
